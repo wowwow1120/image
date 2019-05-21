a472dd3ad92f9731f6bf9925708b3481967ac82f
wowwow1120 <50578092+wowwow1120@users.noreply.github.com>
2019-05-21 09:50:56 +0900

    Add files via upload

--- paste_image.py
+++ paste_image.py
@@ -4,7 +4,36 @@ import PIL.Image as pilimg
 import math
 import operator
 import cv2
+from PIL import Image
 
+import slice_image
+
+
+def update_piece_edge(piece, edge):
+    if edge < 4:
+        return piece, edge
+    if edge % 2 == 1:
+        return np.array(slice_image.mirroring(Image.fromarray(piece))), (edge - 2) % 4
+    return np.array(slice_image.flipping(Image.fromarray(piece))), (edge - 2) % 4
+
+
+def update_final_dict(final_dict, pieces_dict):
+    delete_list = []
+    update_list = []
+    for key, value in final_dict.items():
+        piece1_key = key[0:2]
+        edge1 = int(key[2])
+        piece2_key = key[3:5]
+        edge2 = int(key[5])
+        pieces_dict[piece1_key], new_edge1 = update_piece_edge(pieces_dict[piece1_key], edge1)
+        pieces_dict[piece2_key], new_edge2 = update_piece_edge(pieces_dict[piece2_key], edge2)
+        if edge1 != new_edge1 or edge2 != new_edge2:
+            final_dict[piece1_key + str(new_edge1) + piece2_key + str(new_edge2)] = final_dict[key]
+            del final_dict[key]
+    # for item in delete_list:
+    #     del final_dict[item]
+
+    return sorted(final_dict.items(), key=operator.itemgetter(1))
 
 def paste_img(input_path):
     listdir = os.listdir(input_path)
@@ -23,12 +52,17 @@ def paste_img(input_path):
         pieces_dict[img.split('.')[0]] = img_array
         edge1 = img_array[0, :]
         edge2 = img_array[:, int(img_w - 1)]
-        edge3 = img_array[int(img_h - 1), :]
-        edge4 = img_array[:, 0]
-        edges = [edge1, edge2, edge3[::-1], edge4[::-1]]
+        edge3 = img_array[int(img_h - 1), :][::-1]
+        edge4 = img_array[:, 0][::-1]
+        edge5 = edge1[::-1]
+        edge6 = edge2[::-1]
+        edge7 = edge3[::-1]
+        edge8 = edge4[::-1]
+        edges = [edge1, edge2, edge3, edge4,edge5, edge6, edge7, edge8]
         edges_dict[img.split('.')[0]] = edges
 
     cost_dict = {}
+    tmp_name_list = []
     for key1, edges1 in edges_dict.items():
         for key2, edges2 in edges_dict.items():
             if key1 == key2:
@@ -47,9 +81,13 @@ def paste_img(input_path):
             for idx1 in range(0, len(tmp_edges1)):
                 for idx2 in range(0, len(tmp_edges2)):
                     cost_key = name1 + str(idx1) + name2 + str(idx2)
-                    cost_key_2 = name1 + name2
-                    if cost_key in cost_dict:
+                    idx1_tmp = (idx1 + 4) % 8
+                    idx2_tmp = (idx2 + 4) % 8
+                    cost_key_tmp = name1 + str(idx1_tmp) + name2 + str(idx2_tmp)
+                    if cost_key_tmp in tmp_name_list:
                         continue
+                    tmp_name_list.append(cost_key)
+                    cost_key_2 = name1 + name2
                     cost_dict[cost_key, cost_key_2] = get_distance_between(tmp_edges1[idx1], tmp_edges2[idx2])
 
     cost_dict_sort = sorted(cost_dict.items(), key=operator.itemgetter(1))
@@ -65,6 +103,8 @@ def paste_img(input_path):
 
     print(final_dict)
 
+    final_dict = update_final_dict(final_dict, pieces_dict)
+
     pieces = []
     piece_num_edge = {}
     for key, val in final_dict.items():
@@ -77,42 +117,42 @@ def paste_img(input_path):
         update_piece_edge_dict(piece1_num, piece1_edge, piece_num_edge)
         update_piece_edge_dict(piece2_num, piece2_edge, piece_num_edge)
 
-    list1 = ['1', '2']
-    list2 = ['2', '3']
-    list3 = ['0', '1']
-    list4 = ['0', '3']
-    sorted_val_list = []
-    for i in piece_num_edge.items():
-        sorted_val_list.append(i)
-        sorted_val = sorted(i[1])
-        if sorted_val == list1:
-            pieces.append(i[0])
-        else:
-            continue
-
-    for i in piece_num_edge.items():
-        sorted_val_list.append(i)
-        sorted_val = sorted(i[1])
-        if sorted_val == list2:
-            pieces.append(i[0])
-        else:
-            continue
-
-    for i in piece_num_edge.items():
-        sorted_val_list.append(i)
-        sorted_val = sorted(i[1])
-        if sorted_val == list3:
-            pieces.append(i[0])
-        else:
-            continue
-
-    for i in piece_num_edge.items():
-        sorted_val_list.append(i)
-        sorted_val = sorted(i[1])
-        if sorted_val == list4:
-            pieces.append(i[0])
-        else:
-            continue
+    # list1 = ['1', '2']
+    # list2 = ['2', '3']
+    # list3 = ['0', '1']
+    # list4 = ['0', '3']
+    # sorted_val_list = []
+    # for i in piece_num_edge.items():
+    #     sorted_val_list.append(i)
+    #     sorted_val = sorted(i[1])
+    #     if sorted_val == list1:
+    #         pieces.append(i[0])
+    #     else:
+    #         continue
+    #
+    # for i in piece_num_edge.items():
+    #     sorted_val_list.append(i)
+    #     sorted_val = sorted(i[1])
+    #     if sorted_val == list2:
+    #         pieces.append(i[0])
+    #     else:
+    #         continue
+    #
+    # for i in piece_num_edge.items():
+    #     sorted_val_list.append(i)
+    #     sorted_val = sorted(i[1])
+    #     if sorted_val == list3:
+    #         pieces.append(i[0])
+    #     else:
+    #         continue
+    #
+    # for i in piece_num_edge.items():
+    #     sorted_val_list.append(i)
+    #     sorted_val = sorted(i[1])
+    #     if sorted_val == list4:
+    #         pieces.append(i[0])
+    #     else:
+    #         continue
 
     pieces_in_order = []
     for piece in pieces:
